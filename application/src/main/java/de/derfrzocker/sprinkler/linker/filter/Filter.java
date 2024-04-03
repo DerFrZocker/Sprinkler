68703c508ef47959a686face680e3d58e56e86fd
DerFrZocker <git@derfrzocker.de>
2024-04-03 10:10:12 +0200

    The filter should not be here

--- application/src/main/java/de/derfrzocker/sprinkler/linker/filter/Filter.java
+++
@@ -1,8 +0,0 @@
-package de.derfrzocker.sprinkler.linker.filter;
-
-import de.derfrzocker.sprinkler.data.PullRequest;
-
-public interface Filter {
-
-    boolean isApplicable(PullRequest current, PullRequest found);
-}
